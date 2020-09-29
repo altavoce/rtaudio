b4dcaaa8a4ec6dc739928db992d45e7bc7d6c351
Gilles Degottex <gilles.degottex@gmail.com>
2020-09-29 11:34:56 +0200

    info

--- RtAudio.cpp
+++ RtAudio.cpp
@@ -5432,6 +5432,7 @@ void RtApiWasapi::wasapiThread()
       hr = renderAudioClient->GetBufferSize( &bufferFrameCount );
       if ( FAILED( hr ) ) {
         errorText = "RtApiWasapi::wasapiThread: Unable to retrieve render buffer size.";
+        std::cout << "ERROR: RtApiWasapi::wasapiThread: Unable to retrieve render buffer size." << std::endl;
         specificErrorType = RtAudioError::NO_DEVICES_FOUND;
         goto Exit;
       }
@@ -5513,7 +5514,7 @@ Exit:
   if ( !errorText.empty() )
   {
     errorText_ = errorText;
-    std::cout << errorText_ << std::endl;
+    std::cout << errorText_ << " " << specificErrorType << std::endl;
     error( errorType, specificErrorType );
   }
 }
@@ -10257,6 +10258,7 @@ void RtApi :: error( RtAudioError::Type type, RtAudioError::Type specificErrorTy
       stream_.callbackInfo.isRunning = false; // exit from the thread
       abortStream();
     }
+    std::cout << "Calling call back" << std::endl;
     errorCallback( specificErrorTypeForCallBack, errorMessage, stream_.callbackInfo.userData );
     firstErrorOccurred_ = false;
     return;
